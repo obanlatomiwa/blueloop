0ce174e81c68b33595b9676b6d862f71d38af388
Tomiwa Obanla <obanlatomiwa@gmail.com>
2021-06-07 09:37:18 +0100

    added more tests

--- blueloop/urls.py
+++ blueloop/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url
-from django.urls import path,include
+from django.urls import path, include
 from rest_framework import permissions
-
 
 urlpatterns = [
     path('', include('app.urls')),
